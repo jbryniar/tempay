ff1c327b5423b9904cf1b8a1be12a13ca19be75d
Bryniarski <jbryn565@uwsp.edu>
2019-10-07 17:20:19 -0500

    This is my intial regression of the world value survey.

---
+++ tempaycode.R
@@ -0,0 +1,6 @@
+WVSData <- read.csv(file="C:\\Users\\jbryn565\\OneDrive - UWSP\\DAC101\\WV_US.csv", header=T, sep=",")
+
+summary(WVSData$V23)
+str(WVSData)
+myReg=lm(V23~V59,WVSData)
+summary(myReg)
